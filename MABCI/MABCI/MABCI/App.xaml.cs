7867de0820ef4646b976b227af032cbdfb4448a8
Ricardo Teran <ricardo.teran@alumnos.udg.mx>
2021-09-07 22:59:37 -0500

    Carpeta de proyecto de visual
    Carpeta contenedora

---
+++ MABCI/MABCI/MABCI/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MABCI
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            //MainPage = new MainPage(); //pagina por defecto (no nos sirve xD)
+            MainPage = new NavigationPage(new Login());//esta es nuestra nueva pagina inicial :D
+            //debe ser NavigationPage para poder volver a ella despues :D
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ MABCI/MABCI/MABCI/Login.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Plugin.SimpleAudioPlayer;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MABCI
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Login : ContentPage
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Dashboard());
+        }
+    }
+}
